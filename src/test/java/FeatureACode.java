beb92001f7f76c229aca9b0c8c1b848795aaf961
tariktasci <tsctarik@gmail.com>
2021-02-11 09:27:04 +0100

    learning more git

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -2,5 +2,6 @@ public class FeatureACode {
 
     public static void main(String[] args) {
         System.out.println("Awesome");
+        System.out.println("EU4 is learning more git with review");
     }
 }
