d506ba946d972d8e8ad18c45044be983a97ca4b2
tariktasci <tsctarik@gmail.com>
2021-02-11 11:28:46 +0100

    I try something different :))

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -3,5 +3,7 @@ public class FeatureACode {
     public static void main(String[] args) {
         System.out.println("Awesome");
         System.out.println("EU4 is learning more git with review");
+
+        //I try something different :))
     }
 }
